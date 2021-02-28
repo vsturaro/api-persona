c1e5509000f514884e9613e12b76dce34394b7f5
Vander <vstecnologia@gmail.com>
2021-02-28 08:05:33 -0400

    Criando API pessoas LiveCode DIO

---
+++ src/main/java/com/digitalinovation/apipersona/controller/PersonController.java
@@ -0,0 +1,15 @@
+package com.digitalinovation.apipersona.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController //informar q é um controlador
+@RequestMapping("api/v1/people") //mapeamento com versão
+//teste
+public class PersonController {
+    @GetMapping
+    public String getBook() {
+        return "API Test";
+    }
+}
---
+++ src/test/java/com/digitalinovation/apipersona/ApiPersonaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.digitalinovation.apipersona;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiPersonaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
