aa329dc78d1e0931310bb3a8f4e89c623e0b177d
Vander <vstecnologia@gmail.com>
2021-03-01 14:54:20 -0400

    Developed unit tests for create person

---
+++ src/test/java/com/digitalinovation/apipersona/utils/PhoneUtils.java
@@ -0,0 +1,27 @@
+package com.digitalinovation.apipersona.utils;
+
+import com.digitalinovation.apipersona.dto.request.PhoneDTO;
+import com.digitalinovation.apipersona.entity.Phone;
+import com.digitalinovation.apipersona.enums.PhoneType;
+
+public class PhoneUtils {
+
+    private static final String PHONE_NUMBER = "1199999-9999";
+    private static final PhoneType PHONE_TYPE = PhoneType.MOBILE;
+    private static final long PHONE_ID = 1L;
+
+    public static PhoneDTO createFakeDTO() {
+        return PhoneDTO.builder()
+                .number(PHONE_NUMBER)
+                .type(PHONE_TYPE)
+                .build();
+    }
+
+    public static Phone createFakeEntity() {
+        return Phone.builder()
+                .id(PHONE_ID)
+                .number(PHONE_NUMBER)
+                .type(PHONE_TYPE)
+                .build();
+    }
+}
